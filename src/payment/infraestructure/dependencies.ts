4bd6c1ae69c969f138b9e4c7ec819e3cab02716f
AlbertoSMC72 <223247@ids.upchiapas.edu.mx>
2024-03-03 20:18:48 -0600

    modified:   src/payment/infraestructure/dependencies.ts

--- src/payment/infraestructure/dependencies.ts
+++ src/payment/infraestructure/dependencies.ts
@@ -5,7 +5,7 @@ import { SocketIOPort } from "../../shared/socket/infraestructure/ports/SocketIO
 import { SendDataService } from "../../shared/socket/application/services/sendData.service";
 import { CreatePaymentController } from "./controllers/CreatePayment.controller";
 
-const socketIoPort = new SocketIOPort("http://localhost:4000");
+const socketIoPort = new SocketIOPort("http://52.72.28.83:4000");
 const amqplLib = new AmqpLibPort("amqp://34.193.221.88");
 
 const sendMessageService = new SendMessageService(amqplLib);
